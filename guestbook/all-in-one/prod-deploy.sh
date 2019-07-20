ba9b7b79b80c3444d7ee67143eb7d86ff92c1434
Subhranil Dey <sdey@teamdrg.com>
2019-07-20 23:22:22 +0530

    deplument script for production

--- guestbook/all-in-one/prod-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f prod-namespace.yaml
-kubectl apply -f guestbook-all-in-one.yaml --namespace=production
-kubectl apply -f production-ingress.yaml --namespace=production
