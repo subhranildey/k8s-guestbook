b808a14be498a67a00e44111c5cb6ee3ae96869c
Subhranil Dey <sdey@teamdrg.com>
2019-07-21 23:48:07 +0530

    changed production deployment script name

---
+++ production-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#The following Mandatory Command is required for all deployments for nginx-ingress-controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+
+#GCE-GKE
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
+
+kubectl apply -f guestbook/all-in-one/prod-namespace.yaml
+kubectl apply -f guestbook/all-in-one/guestbook-all-in-one.yaml --namespace=production
+kubectl apply -f guestbook/all-in-one/production-ingress.yaml --namespace=production
