e95f190a2cb4d5cf55ad376a4555e2d3b2522009
Subhranil Dey <sdey@teamdrg.com>
2019-07-31 17:52:22 +0530

    updated global script

--- deploy-guestbook.sh
+++ deploy-guestbook.sh
@@ -22,21 +22,58 @@ while true; do
         * ) echo "Please type P or S";;
     esac
 done
+#------------------------------------Ingress-Controller---------------------------------------
 
-printf "\nDeploying $ENV.......... \n"
+#The following Mandatory Command is required for all deployments for nginx-ingress-controller
+printf "\n  Setting up nginx ingress controller.....\n"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+
+#GCE-GKE
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
+#---------------------------------------------------------------------------------------------
+
+#--------------------------------------ENV Deployment-----------------------------------------
+printf "\n  Deploying $ENV.....\n"
 git clone https://github.com/subhranildey/k8s-guestbook.git $WDIR
-sh $WDIR/$ENV-deploy.sh
+
+#------------------------------------------HPA------------------------------------------------
 
 #To set up HPA we need to set up monitoring, so we are going to install metric-server
 
-echo "To set up HPA we need to set up monitoring, so we are going to install metric-server"
+printf "\n  To set up HPA we need to set up monitoring, so we are going to install metric-server.....\n"
 cd $WDIR
 git clone https://github.com/kubernetes-incubator/metrics-server.git
 
-printf "\nDeploying Metrics Server for feeding in core metrics to HPA..........\n"
+printf "\n  Deploying Metrics Server for feeding in core metrics to HPA.....\n"
 kubectl apply -f $WDIR/metrics-server/deploy/1.8+/
 
-printf "\n deploy HPA.......... \n"
+printf "\n  Deploy HPA.....\n"
 kubectl apply -f $WDIR/hpa.yaml --namespace=$ENV
 
+#-----------------------------------------Deployment----------------------------------------------
+
+kubectl apply -f $WDIR/guestbook/all-in-one/$ENV-namespace.yaml
+kubectl apply -f $WDIR/guestbook/all-in-one/guestbook-all-in-one.yaml --namespace=$ENV
+kubectl apply -f $WDIR/guestbook/all-in-one/$ENV-ingress.yaml
+
+IP=$(kubectl get ing -n $ENV | grep $ENV | awk '{print $3}')
+
+printf "\n  Wait for the $ENV IP...."
+
+until [[ $IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
+    IP=$(kubectl get ing -n $ENV | grep $ENV | awk '{print $3}')
+done
+
+printf "\nMake the below entry to /etc/hosts file.....\n"
+
+if  [ $ENV == "production" ]
+then
+    printf "\n$IP guestbook.mstakx.io\n"
+else
+    printf "\n$IP staging-guestbook.mstakx.io\n"
+fi
+
+#-------------------------------------Cleanup----------------------------------------------------
+printf "\n  Deleting $WDIR.....\n"
+rm -rf $WDIR
 
