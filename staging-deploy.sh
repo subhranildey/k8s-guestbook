70c019bdd536688c08ce32bad05e60eccf4d1226
Subhranil Dey <sdey@teamdrg.com>
2019-07-31 16:53:56 +0530

    updated staging deploy script

--- staging-deploy.sh
+++ staging-deploy.sh
@@ -12,8 +12,12 @@ kubectl apply -f guestbook/all-in-one/staging-ingress.yaml
 
 IP=$(kubectl get ing -n staging | grep staging | awk '{print $3}')
 
+printf "\n Wait for the Staging IP....\n"
+
 until [[ $IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
     IP=$(kubectl get ing -n staging | grep staging | awk '{print $3}')
 done
 
-printf "\nMake the below entry to /etc/hosts file\n\n$IP staging-guestbook.mstakx.io"
+printf "\nMake the below entry to /etc/hosts file \n"
+
+printf "\n$IP staging-guestbook.mstakx.io\n"
